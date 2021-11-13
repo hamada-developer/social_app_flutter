1134f6c3f25418d3e3187b963a37f797ba639fc9
hamadamohamed <hamada.devlop@gmail.com>
2021-11-13 18:14:14 +0200

    test commit from macos

--- lib/modules/bottom_nav/settings/views/screens/edit_screen.dart
+++ lib/modules/bottom_nav/settings/views/screens/edit_screen.dart
@@ -17,7 +17,7 @@ class EditScreen extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: TextButton(
               onPressed: () {},
-              child: Text('update'.toUpperCase()),
+              child: Text('hi hamada'.toUpperCase()),
             ),
           ),
         ],
